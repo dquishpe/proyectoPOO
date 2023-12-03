561f34424532cd31287a11808ef736e55dbd58fa
unknown <dquishpe@espol.edu.ec>
2023-12-03 12:24:56 -0500

    Utilitaria

--- ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Utilitaria.java
+++ ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Utilitaria.java
@@ -4,10 +4,24 @@
  */
 package ec.edu.espol.arboledaquishperiveradomino;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author PERSONAL
  */
 public class Utilitaria {
-    
+    public static ArrayList<Ficha> crearManoJugador(){
+        ArrayList<Ficha> mano= new ArrayList<>();
+        for (int i=0; i<5; i++){
+            Random rd = new Random();
+            int lado1= rd.nextInt(7);
+            int lado2= rd.nextInt(7);
+            Ficha f= new Ficha(lado1, lado2);
+            mano.add(f);           
+        }
+        mano.add(new FichaComodin());
+        return mano;
+    }
 }
