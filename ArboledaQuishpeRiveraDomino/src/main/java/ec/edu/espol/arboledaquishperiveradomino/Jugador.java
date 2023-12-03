7c77acd5c441a738a8ea9f92a8434de46f35545f
unknown <dquishpe@espol.edu.ec>
2023-12-03 11:35:50 -0500

    atributos jugador

--- ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Jugador.java
+++ ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Jugador.java
@@ -4,10 +4,13 @@
  */
 package ec.edu.espol.arboledaquishperiveradomino;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author PERSONAL
  */
 public class Jugador {
-    
+    private String nombre;
+    private ArrayList<Ficha> mano;
 }
