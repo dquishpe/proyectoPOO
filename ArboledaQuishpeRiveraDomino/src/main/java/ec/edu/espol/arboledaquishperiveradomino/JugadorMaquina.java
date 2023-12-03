d9a9e43c2f0dc397afe05d7997137da605098fc5
Domaalva <139288690+Domaalva@users.noreply.github.com>
2023-12-03 12:32:36 -0500

    Add files via upload

---
+++ ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/JugadorMaquina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectofinal1;
+
+/**
+ *
+ * @author domen
+ */
+public class JugadorMaquina extends Jugador{
+    public JugadorMaquina(){
+        super("Maquina", Utilitaria.crearManoJugador());
+    }
+}
+
