cb6fc792917d5c4e755b3d16929751d9eb00bbce
Randy Adley Rivera Bermudez <rariberm@espol.edu.ec>
2023-12-03 11:49:00 -0500

    Agregue metodos de juego

--- ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Juego.java
+++ ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Juego.java
@@ -9,5 +9,27 @@ package ec.edu.espol.arboledaquishperiveradomino;
  * @author spupi
  */
 public class Juego {
+    private ArrayList<Ficha> lineaJuego;
+    private ArrayList<Jugador> jugadores;
     
+    public Juego(){
+        lineaJuego = new ArrayList<>();
+        jugadores = new ArrayList<>();
+    }
+    
+    public void setLineaJuego(ArrayList<Ficha> lineaJuego){
+        this.lineaJuego = lineaJuego;
+    }
+    
+    public void setJugadores(JugadorMaquina maquina){
+        if (jugadores.size() < 2){
+            jugadores.add(maquina);
+        }
+    }
+    
+    public void setJugadores (String nombre){
+        if (jugadores.size() < 2){
+            jugadores.add(new Jugador(nombre));
+        }      
+    }
 }
