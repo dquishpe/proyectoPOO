aa608aa8139753d793b489f6e15d356272c7c5c6
Randy Adley Rivera Bermudez <rariberm@espol.edu.ec>
2023-12-03 14:15:57 -0500

    Ya falta poco

--- ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Juego.java
+++ ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Juego.java
@@ -4,6 +4,8 @@
  */
 package ec.edu.espol.arboledaquishperiveradomino;
 
+import java.util.Scanner;
+
 /**
  *
  * @author spupi
@@ -32,4 +34,51 @@ public class Juego {
             jugadores.add(new Jugador(nombre));
         }      
     }
+    
+    public ArrayList<Ficha> getLineaJuego(){
+        return jugadores;
+    }
+    
+    public ArrayList<Jugador> getJugadores(){
+        return jugadores;
+    }
+    
+    public void agregarJugador(String nombre){
+        Jugador j = new Jugador(nombre, Utilitaria.crearManoJugador());
+        jugadores.add(j);
+    }
+    
+    public int obtenerValorInicioLinea(){
+        return lineaJuego.get(0).getLado1();
+    }
+    
+    public int obtenerValorFinLinea(){
+        return lineaJuego.get(lineaJuego.size()-1).getLado2();
+    }
+    
+    public void mostrarLinea(){
+        if(!lineaJuego.isEmpty()){
+            StringBuilder sb = new StringBuilder();
+            
+            for (int i=0; i < lineaJuego.size()-1; i++){
+                sb.append(lineaJuego.get(i).toString()).append(" - ");
+            }
+            sb.append(lineaJuego.get(lineaJuego.size()-1).toString());
+            System.out.println(sb.toString());
+        }
+    }
+    
+    public boolean agregarFichaLinea(Ficha f, Jugador j){
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
+        
+        if(j instanceof JugadorMaquina){
+            if (f instanceof FichaComodin fComodin){
+                if (lineaJuego.isEmpty()){
+                
+                }
+            }
+        }
+    }
+    
 }
--- ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Jugador.java
+++ ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/Jugador.java
@@ -13,4 +13,6 @@ import java.util.ArrayList;
 public class Jugador {
     private String nombre;
     private ArrayList<Ficha> mano;
+    
+    
 }
