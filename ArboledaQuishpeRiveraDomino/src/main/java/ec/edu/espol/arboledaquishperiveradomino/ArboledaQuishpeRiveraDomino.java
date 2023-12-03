82db75b2ff33637c3dae1bcaf4ec071bb3e1401a
Randy Adley Rivera Bermudez <rariberm@espol.edu.ec>
2023-12-03 10:48:12 -0500

    primer intento para ver si el directorio funciona

---
+++ ArboledaQuishpeRiveraDomino/src/main/java/ec/edu/espol/arboledaquishperiveradomino/ArboledaQuishpeRiveraDomino.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.arboledaquishperiveradomino;
+
+/**
+ *
+ * @author spupi
+ */
+public class ArboledaQuishpeRiveraDomino {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
